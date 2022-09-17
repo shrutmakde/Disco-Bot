49e2ce6d609d29eebf0559b099e3d8474e0b179c
Shrut Makde <66404362+shrutmakde@users.noreply.github.com>
2022-09-18 00:46:47 +0530

    Update DiscoBot.py

--- DiscoBot.py
+++ DiscoBot.py
@@ -8,26 +8,26 @@ intents.members = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
-keywords = ['susu']
+keywords = ['hola']
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!mention me'):
         await message.channel.send(message.author.mention)
     if message.content.startswith("Who is my creator?"):
-        await message.channel.send("Prometheus#2874 made me in 2020. I initially started out as a bot for jokes but now he works on me part time.")
+        await message.channel.send("Shrut made me in 2020. I initially started out as a bot for jokes but now he works on me part time.")
     if message.content.startswith("I love you"):
         await message.channel.send("I love you too!")
     for i in range(len(keywords)):
         if keywords[i] in message.content:
          for j in range(1):
-                await message.channel.send("susu")
+                await message.channel.send("hola")
     await client.process_commands(message)
 
 @client.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server! I am a bot made by shivansh entirely in python. Nice to meet you!')
+    await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server! I am a bot made by Shrut entirely in python. Nice to meet you!')
 
 @client.command(help='pings phoenix')
 async def pingphoenix(ctx):
